1482835e062e50f9aafbfebd8a2ff013ddb64431
cainy-a <cainatkinson98@gmail.com>
2020-12-02 22:30:55 +0000

    Add moar stuff

---
+++ ZshrcGenerator/Plugin.cs
@@ -0,0 +1,16 @@
+﻿namespace ZshrcGenerator
+{
+	public class Plugin
+	{
+		public bool   BuiltIn;
+		public string GithubLink;
+		public string Name;
+
+		public Plugin(string name, bool builtIn, string githubLink)
+		{
+			BuiltIn    = builtIn;
+			GithubLink = githubLink;
+			Name       = name;
+		}
+	}
+}
