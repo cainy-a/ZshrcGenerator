f1d5dd08bba59ba118573abf9cdc1c8044530939
cainy-a <cainatkinson98@gmail.com>
2020-12-03 19:41:36 +0000

    Basically done, just needs testing to make sure now

---
+++ ZshrcGenerator/Alias.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ZshrcGenerator
+{
+	public class Alias
+	{
+		/// <summary>
+		/// Represents a zsh alias.
+		/// </summary>
+		/// <param name="key">The alias keyword</param>
+		/// <param name="replacement">The replacement for the keyword</param>
+		/// <param name="suffix">Whether to look for the keyword at the end</param>
+		/// <param name="function">Whether the alias is a function</param>
+		/// <param name="permissive">If enabled and suffix and function are both true, function will take priority and no exception will be thrown.</param>
+		public Alias(string key, string replacement, bool suffix = false, bool function = false, bool permissive = false)
+		{
+			if (!permissive)
+			{
+				if (suffix && function)
+					throw new Exception("Alias cannot be suffix and function simultaneously");
+			}
+			else if (function) suffix = false;
+
+			Key         = key;
+			Replacement = replacement;
+			Suffix      = suffix;
+			Function    = function;
+		}
+
+		public string Key;
+		public string Replacement;
+		public bool   Suffix;
+		public bool   Function;
+	}
+}
